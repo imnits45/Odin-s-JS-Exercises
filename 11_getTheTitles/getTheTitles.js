393d99d0ba549a821328aa1b9a95434f156940b3
imnits45 <imnits45>
2024-04-27 03:05:01 +0530

    Completed challenge 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    const titles = [];
+    for(let i in books)
+    {
+        titles.push(books[i].title);
+    }
+    return titles;
 };
 
 // Do not edit below this line
