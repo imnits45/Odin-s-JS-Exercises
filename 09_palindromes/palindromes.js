c9ea3fa7fdc586e09effa0a0642c6b76b64738fa
imnits45 <imnits45>
2024-04-27 02:36:36 +0530

    Completed challenge 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let original = "";
+    for(let i = 0; i < str.length; i++) {
+        let char = str[i];
+        if((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9'))
+            original += char.toLowerCase();
+    }
+    
+    let reversed = "";
+    for(let i = str.length - 1; i >= 0; i--) {
+        let char = str[i];
+        if((char >= 'a' && char <= 'z') || (char >= 'A' && char <= 'Z') || (char >= '0' && char <= '9'))
+            reversed += char.toLowerCase();
+    }
+    
+    return original === reversed;
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
