a53040cc5bd863ebbc22f5224819b68267339406
imnits45 <imnits45>
2024-04-26 21:35:13 +0530

    Completed Challenge 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(typeof(num1) !== "number" || typeof(num2) !== "number" || Math.min(num1,num2) < 0)
+        return "ERROR";
+    const maxNum = Math.max(num1,num2);
+    const minNum = Math.min(num1,num2);
+    let sum = 0;
+    for(let i = minNum; i <= maxNum; i++)
+        sum += i;
+    return sum;
 };
 
 // Do not edit below this line
