f6c3a372284af2914bca797c5621559579993957
imnits45 <imnits45>
2024-04-27 02:57:16 +0530

    Completed Challenge 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    let num = parseInt(n);
+    if(num < 0)
+        return "OOPS";
+    let f1 = 0;
+    let f2 = 1;
+    if(num <2)
+        return num;
+    let ans = 0;
+    for(let i = 2; i <= num; i++)
+    {
+        ans = f1 + f2;
+        f1 = f2;
+        f2 = ans;
+    }
+    return ans;
 };
 
 // Do not edit below this line
